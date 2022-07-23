55bba1dd08f1caec9c4aed2f3617a5e5a16b2f02
Iduwaofficial <109781073+Iduwaofficial@users.noreply.github.com>
2022-07-23 12:32:35 +0530

    Upchanges.js

--- settings.js
+++ settings.js
@@ -21,19 +21,19 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['94763427338']
-global.premium = ['94763427338']
-global.ownernomer = '94763427338'
-global.ownername = '៚ᴠɪʜͥᴀɴͣɢͫᴀ ʏᴛ͢⁸⁵⁴¹⃟✦⃝̮̮〽️'
-global.botname = 'VIHANGA MD BOT'
-global.footer = 'VIHANGA MD'
-global.cap = '*Genarated By Vihanga Md*'
+global.owner = ['723418093']
+global.premium = ['723418093']
+global.ownernomer = '94723418093'
+global.ownername = '𝙸𝙳𝚄𝚁𝙰𝙽𝙶𝙰 𝙱𝙾𝚃⁸⁵⁴¹⃟✦⃝̮̮〽️'
+global.botname = '𝙸𝙳𝚄𝚁𝙰𝙽𝙶𝙰 𝙱𝙾𝚃'
+global.footer = '𝙸𝙳𝚄𝚁𝙰𝙽𝙶𝙰 𝙱𝙾𝚃'
+global.cap = '*Genarated By 𝙸𝙳𝚄𝚁𝙰𝙽𝙶𝙰 𝙼𝙳*'
 global.ig = 'https://github.com/vihangabot'
-global.region = 'Sri Lanka, Sabaragamuwa, Embilipitiya'
+global.region = 'Sri Lanka, 𝙺𝚄𝚁𝚄𝙽𝙴𝙶𝙰𝙻𝙰, 𝙼𝙰𝚆𝙰𝚃𝙷𝙰𝙶𝙰𝙼𝙰'
 global.sc = 'https://github.com/vihangabot'
 global.myweb = 'https://chat.whatsapp.com/DW8hwTRNnolEibX9dhIMWT'
-global.packname = 'Vihanga Md Sticker Bot'
-global.author = '៚ᴠɪʜͥᴀɴͣɢͫᴀ ʏᴛ͢⁸⁵⁴¹⃟✦⃝̮̮〽️'
+global.packname = '𝙸𝙳𝚄𝚁𝙰𝙽𝙶𝙰  Md Sticker Bot'
+global.author = '៚𝙸𝙳𝚄𝚁𝙰𝙽𝙶𝙰 𝙱𝙾𝚃⁵⁴¹⃟✦⃝̮̮〽️'
 global.autobio = 'true'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
